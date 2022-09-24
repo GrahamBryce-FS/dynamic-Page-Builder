1d97801a85ce8cccbffb430fd382f048f603128f
Christian <ccampbell@sapphirebd.com>
2022-09-24 09:58:00 -0500

    Clean up

--- DynamicPageBuilder/js/main.js
+++ DynamicPageBuilder/js/main.js
@@ -66,51 +66,31 @@ class Main {
   createObjects() {
     console.log("creating objects");
     for (let i = 0; i < movies.length; i++) {
-      // text += cars[i] + "<br>";
-      // console.log(movies[i].title,movies[i].rating,movies[i].duration,movies[i].imageFile)
-      // let aliens = new Movie(movies[0].title,movies[0].rating,movies[0].duration,movies[0].imageFile)
-      // let matrix = new Movie(movies[1].title,movies[1].rating,movies[1].duration,movies[1].imageFile)
-      // let endgame = new Movie(movies[2].title,movies[2].rating,movies[2].duration,movies[2].imageFile)
       var x = new Movie(movies[i].title,movies[i].rating,movies[i].duration,movies[i].imageFile)
-      // console.log(x);
       this.moviesArray.push(x);
-      
     }
-
-    // console.log(this.moviesArray)
   }
 
   // Function to build the DOM and sets up event Listeners for the Image and next button
   buildDom() {
     console.log("building DOM");
-
     // adding a buttton
     const app = this; 
     let btn = document.createElement("button");
     btn.innerHTML = "Next";
+    // adding click event 
     btn.onclick = function () {
-      // alert("Button is clicked");
-      // showMovie()
-      // main.showMovie()
-      // console.log(app)
       app.showMovie()
-
     };
     document.body.appendChild(btn);
 
-
+    // adding div to display content. 
     let trayDiv = document.createElement("div");
     trayDiv.id = 'movietray';
-
     document.body.appendChild(trayDiv);
 
 
-
-    // let imgTray = document.createElement("img");
-    // imgTray.id = 'imgTray';
-    // trayDiv.appendChild(imgTray);
     trayDiv.appendChild(this.image);
-
 
     let title = document.createElement("p");
     title.id = 'title';
@@ -124,100 +104,36 @@ class Main {
     duration.id = 'duration';
     trayDiv.appendChild(duration);
    
-
-
-    //// Add a movie tray  
-
-
-
-
-    // let btn = document.createElement("button");
-    // btn.innerHTML = "Click Me";
-    // document.body.appendChild(btn);
-
-
-    // this.createMovieBlock(this.moviesArray[0])
-
-    // for (let i = 0; i < this.moviesArray.length; i++) {
-    // }
-
-    // const newDiv = document.createElement("div");
-    // const newContent = document.createTextNode("Hi there and greetings!");
-    // newDiv.appendChild(newContent);
-    // const currentDiv = document.getElementById("div1");
-    // document.body.insertBefore(newDiv, currentDiv);
-  }
-
-  fillInvars(){
-    // var newDiv = document.createElement("div");
-    // var newContent = document.createTextNode(movie.title);
-    // newDiv.appendChild(newContent);
-    // var currentDiv = document.getElementById("div1");
-    // document.body.insertBefore(newDiv, currentDiv);
-    console.log('Fill in vars')
-
-    var title = document.getElementById('title')
-    // title.innerHTML = 'boogers';
-    var rating = document.getElementById('rating')
-    var duration = document.getElementById('duration')
-    title.innerHTML = this.movieTitle;
-    rating.innerHTML = this.movieRating;
-    duration.innerHTML = Utils.convertTime(this.movieDuration);
-
-    // console.log(this.movieNumber);
-    this.image.src = 'images/'+ this.moviesArray[this.movieNumber ].image
-
-
-// this.movieTitle;
-// this.movieRating;
-// this.movieDuration;
-
-
   }
 
   // This function updates the output to show the correct movie information and image
   // It should be called when next button is clicked and also initially so a movie is seen before button is clicked the first time
   showMovie() {
-    // console.log('Show Movies')
-    // console.log(this.movieNumber)
-    // console.log(this.moviesArray.length)
+    console.log('Show Movies')
 
+    // CHECK THAT MOVIE NUMBER IS IN RANGe 
     if(this.movieNumber > this.moviesArray.length - 1 ){
       this.movieNumber = 0; 
     }
 
-    // console.log()
 
     var movie = this.moviesArray[this.movieNumber]
-    // console.log(movie)
+
     this.movieTitle = movie.title
     this.movieDuration = movie.duration
     this.movieRating = movie.rating
 
-    // this.image.src = movie.imageFile
+    var title = document.getElementById('title')
+    var rating = document.getElementById('rating')
+    var duration = document.getElementById('duration')
+    title.innerHTML = this.movieTitle;
+    rating.innerHTML = this.movieRating;
+    duration.innerHTML = Utils.convertTime(this.movieDuration);
+    this.image.src = 'images/'+ this.moviesArray[this.movieNumber ].image
 
-    // console.log(this.movieTitle)
-    // console.log(this.movieDuration)
-    // console.log(this.movieRating)
-
-
-    // console.log("show movie");
-
-    // update current movie index//
-    // update dom with correct info 
-
-    this.fillInvars()
-    // Use utility class to convert times to 00:00 (hours:minutes) format
+    // increment movie number
     this.movieNumber++;
-    
-    // if(this.movieNumber > this.moviesArray.length){
-    //   this.movieNumber = 0; 
-    // }
-
-
-    ////
-
-    
+        
 
   }
 }
